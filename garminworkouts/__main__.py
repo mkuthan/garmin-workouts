2314b5d5a9533872f9602413b52c828108782425
osalgado <osalgado@ikerlan.es>
2023-05-05 07:23:33 +0200

    Manage planning exceptions

--- garminworkouts/__main__.py
+++ garminworkouts/__main__.py
@@ -43,10 +43,12 @@ def command_import(args):
     race = account.race
     
     if not workout_files:
-        planning = configreader.read_config(r'planning.yaml')
-        workout_files = glob.glob(planning[args.workout]['workouts'])
-        race =  date(planning[args.workout]['year'], planning[args.workout]['month'], planning[args.workout]['day'])    
-
+        try: 
+            planning = configreader.read_config(r'planning.yaml')
+            workout_files = glob.glob(planning[args.workout]['workouts'])
+            race =  date(planning[args.workout]['year'], planning[args.workout]['month'], planning[args.workout]['day'])    
+        except:
+            print(args.workout + ' not found in planning, please check "planning.yaml"')
     workout_configs = [configreader.read_config(workout_file) for workout_file in workout_files]
     target = configreader.read_config(r'pace.yaml')
     workouts = [RunningWorkout(workout_config, target, account.vV02, account.fmin, account.fmax) for workout_config in workout_configs]
