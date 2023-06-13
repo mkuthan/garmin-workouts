e97ff137b7976058502d76f5921df6e6e5697e47
osalgado <osalgado@ikerlan.es>
2023-06-13 14:32:31 +0200

    Use heart reate zones

--- garminworkouts/config/generator.py
+++ garminworkouts/config/generator.py
@@ -2,7 +2,7 @@ def recovery_step_generator(duration):
     step = {}
     step['type'] = 'run'
     step['duration'] = duration
-    step['target'] = 'RECOVERY_PACE'
+    step['target'] = 'RECOVERY_HEART_RATE'
     step['description'] = 'Recovery pace'
 
     return step
@@ -12,7 +12,7 @@ def aerobic_step_generator(duration):
     step = {}
     step['type'] = 'run'
     step['duration'] = duration
-    step['target'] = 'AEROBIC_PACE'
+    step['target'] = 'AEROBIC_HEART_RATE'
     step['description'] = 'Aerobic pace'
 
     return step
@@ -32,7 +32,7 @@ def lr_step_generator(duration):
     step = {}
     step['type'] = 'run'
     step['duration'] = duration
-    step['target'] = 'LONG_RUN_PACE'
+    step['target'] = 'LONG_RUN_HEART_RATE'
     step['description'] = 'Long run pace'
 
     return step
@@ -42,7 +42,7 @@ def marathon_step_generator(duration):
     step = {}
     step['type'] = 'run'
     step['duration'] = duration
-    step['target'] = 'MARATHON_PACE'
+    step['target'] = 'MARATHON_HEART_RATE'
     step['description'] = 'Marathon pace run'
 
     return step
@@ -72,7 +72,7 @@ def cooldown_step_generator(duration):
     step = {}
     step['type'] = 'cooldown'
     step['duration'] = duration
-    step['target'] = 'AEROBIC_PACE'
+    step['target'] = 'AEROBIC_HEART_RATE'
     step['description'] = 'Aerobic pace'
 
     return step
