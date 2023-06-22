ced73fcd2ef8a504020fe4d0723f9a9cb62ae672
osalgado <osalgado@ikerlan.es>
2023-06-22 23:14:55 +0200

    Add export function corrections

--- garminworkouts/models/running_workout.py
+++ garminworkouts/models/running_workout.py
@@ -1,5 +1,4 @@
 import json
-import datetime
 
 from garminworkouts.models.duration import Duration
 from garminworkouts.models.power import Power
@@ -106,26 +105,68 @@ POOL_LENGTHS = {
 
 
 @staticmethod
+def target_extraction(step_json, step):
+    end_condition = step_json['endCondition']['conditionTypeKey']
+    if end_condition == 'time':
+        step['duration'] = str(timedelta(seconds=int(step_json['endConditionValue'])))
+    elif end_condition == 'distance':
+        step['duration'] = str(float(step_json['endConditionValue'])/1000) + 'km'
+    elif end_condition == 'reps':
+        step['duration'] = str(step_json['endConditionValue']) + 'reps'
+    elif end_condition == 'heart.rate':
+        step['duration'] = str(step_json['endConditionValue']) + 'ppm' + step_json['endConditionCompare']
+    elif end_condition != 'lap.button':
+        print(end_condition, step_json['endConditionValue'])
+    return step
+
+
+@staticmethod
+def weight_extraction(step_json, step):
+    if ('weightValue' in step_json) and (step_json['weightValue'] is not None):
+        if step_json['weightUnit']['unitKey'] == 'kilogram':
+            step['weight'] = str(step_json['weightValue']) + 'kg'
+        elif step_json['weightUnit']['unitKey'] == 'pound':
+            step['weight'] = str(step_json['weightValue']) + 'pound'
+        else:
+            print(step_json['weightUnit']['unitKey'])
+    return step
+
+
+@staticmethod
 def step_extraction(step_json):
     if step_json['stepType']['stepTypeKey'] != 'repeat':
         step = {}
         step['type'] = step_json['stepType']['stepTypeKey']
 
-        if step_json['endCondition']['conditionTypeKey'] == 'time':
-            step['duration'] = str(timedelta(seconds=int(step_json['endConditionValue'])))
-        elif step_json['endCondition']['conditionTypeKey'] == 'distance':
-            step['duration'] = str(float(step_json['endConditionValue'])/1000) + 'km'
+        step = target_extraction(step_json, step)
 
         step['target'] = {}
-        step['target']['type'] = step_json['targetType']['workoutTargetTypeKey']
-        if step['target']['type'] == 'pace.zone':
-            step['target']['min'] = str(timedelta(seconds=int(1000 / float(step_json['targetValueOne']))))[2:]
-            step['target']['max'] = str(timedelta(seconds=int(1000 / float(step_json['targetValueTwo']))))[2:]
-        elif step['target']['type'] == 'cadence':
-            step['target']['min'] = str(int(step_json['targetValueOne']))
-            step['target']['max'] = str(int(step_json['targetValueTwo']))
+        step['target']['type'] = 'no.target'
+        if ('targetType' in step_json) and (step_json['targetType'] is not None):
+            step['target']['type'] = step_json['targetType']['workoutTargetTypeKey']
+            if step['target']['type'] == 'pace.zone':
+                step['target']['min'] = str(timedelta(seconds=int(1000 / float(step_json['targetValueOne']))))[2:]
+                step['target']['max'] = str(timedelta(seconds=int(1000 / float(step_json['targetValueTwo']))))[2:]
+            elif step['target']['type'] == 'cadence' or step['target']['type'] == 'heart.rate.zone':
+                step['target']['min'] = str(int(step_json['targetValueOne']))
+                step['target']['max'] = str(int(step_json['targetValueTwo']))
+            elif step['target']['type'] != 'lap.button' and step['target']['type'] != 'no.target':
+                print(step['target']['type'])
+                print(step)
 
         step['description'] = step_json['description'] if step_json['description'] else ''
+
+        step = weight_extraction(step_json, step)
+
+        if ('equipmentType' in step_json) and (
+            step_json['equipmentType'] is not None) and (
+             step_json['equipmentType']['equipmentTypeKey'] is not None):
+            print(step_json['equipmentType'])
+            step['equipment'] = step_json['equipmentType']['equipmentTypeKey']
+        if ('category' in step_json) and (step_json['category'] is not None):
+            step['category'] = step_json['category']
+        if ('exerciseName' in step_json) and (step_json['exerciseName'] is not None):
+            step['exerciseName'] = step_json['exerciseName']
 
         return step
 
@@ -165,7 +206,7 @@ class Workout(object):
         self.plan = plan
         self.race = race
 
-        self.duration = datetime.timedelta(seconds=0)
+        self.duration = timedelta(seconds=0)
         self.mileage = 0
         self.tss = 0
         self.ratio = 0
@@ -204,7 +245,7 @@ class Workout(object):
         except ValueError:
             self.ratio = 0
 
-        self.duration = datetime.timedelta(seconds=sec)
+        self.duration = timedelta(seconds=sec)
         self.mileage = round(meters/1000, 2)
         self.tss = round(sec/3600 * (self.ratio * 0.89) ** 2 / 100)
 
@@ -470,7 +511,7 @@ class Workout(object):
     def _get_target_value(self, target, key):
         if isinstance(target, dict):
             target_type = target['type']
-            target_value = target[key] if key in 'target' else None
+            target_value = target[key] if key in 'target' else int(0)
         else:
             target_type = self.target[target]['type']
             target_value = self.target[target][key]
