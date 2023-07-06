9d09a9d3b471953ce25bb2c2cbaedd74aea30984
osalgado <osalgado@ikerlan.es>
2023-07-06 09:47:48 +0200

    Update

--- sample_account.py
+++ sample_account.py
@@ -1,9 +1,14 @@
 # save this as account.py
 from datetime import date
+from garminworkouts.models.pace import Pace
+from garminworkouts.models.power import Power
 
 USERNAME = 'enter username here'
 PASSWORD = 'enter password here'
-race = date(YYYY, MM, DD)
-vV02 = xxx # (int) seconds/km
-fmin = xxx # resting heart rate
-fmax = xxx # max heart rate
+locale = 'en-US'
+race = date(2023, 7, 5)
+vV02 = Pace('5:00')
+fmin = int(60)
+fmax = int(200)
+rFTP = Power('400w')
+cFTP = Power('300w')
