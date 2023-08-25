7fb98466dde0c4a1947855afd52bbe007215ade4
osalgado <osalgado@ikerlan.es>
2023-08-25 08:53:43 +0200

    Update failing test

--- tests/models/test_types.py
+++ tests/models/test_types.py
@@ -4,8 +4,10 @@ import account
 from garminworkouts.garmin.garminclient import GarminClient
 from garminworkouts.models.fields import get_step_type, get_sport_type, get_end_condition
 from garminworkouts.models.fields import get_intensity_type, get_target_type, get_equipment_type, get_stroke_type
-from garminworkouts.models.fields import _STEP_TYPE, _SPORT_TYPE, _STROKE_TYPE, _CONDITION_TYPE
-from garminworkouts.models.fields import _INTENSITY_TYPE, _WORKOUT_TARGET, _EQUIPMENT_TYPE
+from garminworkouts.models.fields import _STEP_TYPE_ID, _STEP_TYPE_KEY, _SPORT_TYPE_KEY, _SPORT_TYPE_ID
+from garminworkouts.models.fields import _STROKE_TYPE_KEY, _STROKE_TYPE_ID, _CONDITION_TYPE_KEY, _CONDITION_TYPE_ID
+from garminworkouts.models.fields import _INTENSITY_TYPE_ID, _INTENSITY_TYPE_KEY, _WORKOUT_TARGET_ID, _WORKOUT_TARGET_KEY,
+from garminworkouts.models.fields import _EQUIPMENT_TYPE_ID, _EQUIPMENT_TYPE_KEY
 
 
 def _garmin_client() -> GarminClient:
@@ -27,33 +29,26 @@ class TypesTestCase(unittest.TestCase):
                     with self.subTest(type):
                         elem: dict = types[type][i]
                         if type == "workoutStepTypes":
-                            self.assertEqual(elem[f"{_STEP_TYPE}Id"],
-                                             get_step_type(elem[f"{_STEP_TYPE}Key"])[
-                                                 f"{_STEP_TYPE}Id"])
+                            self.assertEqual(elem[_STEP_TYPE_ID],
+                                             get_step_type(elem[_STEP_TYPE_KEY])[_STEP_TYPE_ID])
                         elif type == "workoutSportTypes":
-                            self.assertEqual(elem[f"{_SPORT_TYPE}Id"],
-                                             get_sport_type(elem[f"{_SPORT_TYPE}Key"])[
-                                                 f"{_SPORT_TYPE}Id"])
+                            self.assertEqual(elem[_SPORT_TYPE_ID],
+                                             get_sport_type(elem[_SPORT_TYPE_KEY])[_SPORT_TYPE_ID])
                         elif type == "workoutConditionTypes":
-                            self.assertEqual(elem[f"{_CONDITION_TYPE}Id"],
-                                             get_end_condition(elem[f"{_CONDITION_TYPE}Key"])[
-                                                 f"{_CONDITION_TYPE}Id"])
+                            self.assertEqual(elem[_CONDITION_TYPE_ID],
+                                             get_end_condition(elem[_CONDITION_TYPE_KEY])[_CONDITION_TYPE_ID])
                         elif type == "workoutIntensityTypes":
-                            self.assertEqual(elem[f"{_INTENSITY_TYPE}Id"],
-                                             get_intensity_type(elem[f"{_INTENSITY_TYPE}Key"])[
-                                                 f"{_INTENSITY_TYPE}Id"])
+                            self.assertEqual(elem[_INTENSITY_TYPE_ID],
+                                             get_intensity_type(elem[_INTENSITY_TYPE_KEY])[_INTENSITY_TYPE_ID])
                         elif type == "workoutTargetTypes":
-                            self.assertEqual(elem[f"{_WORKOUT_TARGET}Id"],
-                                             get_target_type(elem[f"{_WORKOUT_TARGET}Key"])[
-                                                 f"{_WORKOUT_TARGET}Id"])
+                            self.assertEqual(elem[_WORKOUT_TARGET_ID],
+                                             get_target_type(elem[_WORKOUT_TARGET_KEY])[_WORKOUT_TARGET_ID])
                         elif type == "workoutEquipmentTypes":
-                            self.assertEqual(elem[f"{_EQUIPMENT_TYPE}Id"],
-                                             get_equipment_type(elem[f"{_EQUIPMENT_TYPE}Key"])[
-                                                 f"{_EQUIPMENT_TYPE}Id"])
+                            self.assertEqual(elem[_EQUIPMENT_TYPE_ID],
+                                             get_equipment_type(elem[_EQUIPMENT_TYPE_KEY])[_EQUIPMENT_TYPE_ID])
                         elif type == "workoutStrokeTypes":
-                            self.assertEqual(elem[f"{_STROKE_TYPE}Id"],
-                                             get_stroke_type(elem[f"{_STROKE_TYPE}Key"])[
-                                                 f"{_STROKE_TYPE}Id"])
+                            self.assertEqual(elem[_STROKE_TYPE_ID],
+                                             get_stroke_type(elem[_STROKE_TYPE_KEY])[_STROKE_TYPE_ID])
 
 
 if __name__ == "__main__":
