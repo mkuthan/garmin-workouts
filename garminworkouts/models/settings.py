4930364acc98549df6d863a329c1c8b1ba976e72
osalgado <osalgado@ikerlan.es>
2023-12-05 17:12:44 +0100

    Refactor settings

--- garminworkouts/models/settings.py
+++ garminworkouts/models/settings.py
@@ -8,46 +8,53 @@ from garminworkouts.models.note import Note
 
 
 def settings(args):
-    if isinstance(args.trainingplan, tuple):
-        args.trainingplan = ''.join(args.trainingplan)
-
-    workout_files: list[str] = glob.glob(args.trainingplan)
-    plan = str('')
-    race: date = date.today()
     try:
         planning: dict = configreader.read_config(os.path.join('.', 'events', 'planning', 'planning.yaml'))
     except FileNotFoundError:
-        print('Planning does not exist')
+        print('Planning is not defined')
         planning = {}
 
-    if not workout_files:
-        try:
-            workout_files = glob.glob(planning[args.trainingplan]['workouts'])
-            race = date(planning[args.trainingplan]['year'],
-                        planning[args.trainingplan]['month'],
-                        planning[args.trainingplan]['day'])
-            plan: str = args.trainingplan
-        except KeyError:
-            os.path.exists(args.trainingplan)
-            if '\\' not in args.trainingplan:
-                if 'year' in planning[args.trainingplan]:
-                    print(args.trainingplan + ' not found in planning, please check "planning.yaml"')
+    if isinstance(args.trainingplan, tuple):
+        args.trainingplan = ''.join(args.trainingplan)
 
-    workout_configs: list = [configreader.read_config(workout_file) for workout_file in workout_files]
-    target: dict = configreader.read_config(r'target.yaml')
-    combined: list[Workout | Note] = [Workout(workout_config,
-                                              target,
-                                              account.vV02,
-                                              account.fmin,
-                                              account.fmax,
-                                              account.flt,
-                                              account.rFTP,
-                                              account.cFTP,
-                                              plan,
-                                              race) if 'content' not in workout_config else Note(workout_config)
-                                      for workout_config in workout_configs]
+    if args.trainingplan in planning:
+        workout_files = glob.glob(planning[args.trainingplan]['workouts'])
+        if 'year' in planning[args.trainingplan]:
+            race = date(
+                planning[args.trainingplan]['year'],
+                planning[args.trainingplan]['month'],
+                planning[args.trainingplan]['day']
+                )
+        else:
+            race: date = date.today()
+        plan: str = args.trainingplan
+    elif os.path.isfile(args.trainingplan):
+        workout_files: list[str] = glob.glob(args.trainingplan)
+        plan = str('')
+        race: date = date.today()
+    else:
+        print(args.trainingplan + ' not found in planning, please check "planning.yaml"')
+        return [], [], ''
 
-    notes = [w for w in combined if isinstance(w, Note)]
-    workouts = [w for w in combined if isinstance(w, Workout)]
+    try:
+        workout_configs: list = [configreader.read_config(workout_file) for workout_file in workout_files]
+        target: dict = configreader.read_config(r'target.yaml')
+        combined: list[Workout | Note] = [
+            Workout(
+                workout_config,
+                target,
+                account.vV02,
+                account.fmin,
+                account.fmax,
+                account.flt,
+                account.rFTP,
+                account.cFTP,
+                plan, race
+                ) if 'content' not in workout_config else Note(workout_config) for workout_config in workout_configs]
 
-    return workouts, notes, plan
+        notes = [w for w in combined if isinstance(w, Note)]
+        workouts = [w for w in combined if isinstance(w, Workout)]
+
+        return workouts, notes, plan
+    except Exception:
+        return [], [], ''
