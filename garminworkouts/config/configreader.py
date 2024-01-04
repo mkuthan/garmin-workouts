167283b4cf2434bda1d45230898ec720a121eb29
osalgado <osalgado@ikerlan.es>
2024-01-04 22:28:19 +0100

    Remove excel

--- garminworkouts/config/configreader.py
+++ garminworkouts/config/configreader.py
@@ -1,13 +1,9 @@
 import yaml
 
-from garminworkouts.config.excelparser import excel_to_yaml
 from garminworkouts.config.includeloader import IncludeLoader
 
 
 def read_config(filename) -> dict:
-    if "xls" in filename.split(".")[-1]:
-        filename = excel_to_yaml(filename)
-
     with open(filename, 'r') as f:
         data: dict = yaml.load(f, IncludeLoader)
         if 'steps' in data and len(data['steps']) == 1 and isinstance(data['steps'][0], list):
