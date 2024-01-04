441453fe98ecaf525d9f5ff38aded10958c17db6
osalgado <osalgado@ikerlan.es>
2024-01-04 22:14:21 +0100

    Add tests

--- tests/config/test_generator.py
+++ tests/config/test_generator.py
@@ -144,7 +144,7 @@ class MyTestCase(unittest.TestCase):
 
         self.assertDictEqual(config, expected_config)
 
-    def test_race_generator(self) -> None:
+    def test_race_steps_generator(self) -> None:
         self.assertEqual(generator.race_steps_generator(z1='5km', description=''),
                          [{'type': 'interval', 'duration': '5km', 'target': 'HEART_RATE_ZONE_1', 'description': ''}])
         self.assertEqual(generator.race_steps_generator(z2='5km', description=''),
@@ -153,6 +153,89 @@ class MyTestCase(unittest.TestCase):
                          [{'type': 'interval', 'duration': '5km', 'target': 'HEART_RATE_ZONE_3', 'description': ''}])
         self.assertEqual(generator.race_steps_generator(z4='5km', description=''),
                          [{'type': 'interval', 'duration': '5km', 'target': 'HEART_RATE_ZONE_4', 'description': ''}])
+
+    def test_race_generator(self) -> None:
+        self.assertEqual(generator.race_generator('5km', 20),
+                         generator.race_steps_generator(z4='5km', description='5k race'))
+        self.assertEqual(generator.race_generator('5km', 25),
+                         generator.race_steps_generator(z3='1km', z4='4km', description='5k race'))
+        self.assertEqual(generator.race_generator('5km', 30),
+                         generator.race_steps_generator(z3='2km', z4='3km', description='5k race'))
+        self.assertEqual(generator.race_generator('5km', 40),
+                         generator.race_steps_generator(z3='3km', z4='2km', description='5k race'))
+        self.assertEqual(generator.race_generator('6km', 20),
+                         generator.race_steps_generator(z4='6km', description='6k race'))
+        self.assertEqual(generator.race_generator('6km', 25),
+                         generator.race_steps_generator(z3='1km', z4='5km', description='6k race'))
+        self.assertEqual(generator.race_generator('6km', 30),
+                         generator.race_steps_generator(z3='2km', z4='4km', description='6k race'))
+        self.assertEqual(generator.race_generator('6km', 36),
+                         generator.race_steps_generator(z3='3km', z4='3km', description='6k race'))
+        self.assertEqual(generator.race_generator('6km', 40),
+                         generator.race_steps_generator(z3='4km', z4='2km', description='6k race'))
+        self.assertEqual(generator.race_generator('10km', 30),
+                         generator.race_steps_generator(z3='4km', z4='6km', description='10k race'))
+        self.assertEqual(generator.race_generator('10km', 40),
+                         generator.race_steps_generator(z3='5km', z4='5km', description='10k race'))
+        self.assertEqual(generator.race_generator('10km', 50),
+                         generator.race_steps_generator(z3='6km', z4='4km', description='10k race'))
+        self.assertEqual(generator.race_generator('10km', 60),
+                         generator.race_steps_generator(z3='7km', z4='3km', description='10k race'))
+        self.assertEqual(generator.race_generator('10km', 70),
+                         generator.race_steps_generator(z3='8km', z4='2km', description='10k race'))
+        self.assertEqual(generator.race_generator('15km', 60),
+                         generator.race_steps_generator(z3='10km', z4='5km', description='15k race'))
+        self.assertEqual(generator.race_generator('15km', 75),
+                         generator.race_steps_generator(z3='11km', z4='4km', description='15k race'))
+        self.assertEqual(generator.race_generator('15km', 90),
+                         generator.race_steps_generator(z3='12km', z4='3km', description='15k race'))
+        self.assertEqual(generator.race_generator('15km', 100),
+                         generator.race_steps_generator(z3='13km', z4='2km', description='15k race'))
+        self.assertEqual(generator.race_generator('20km', 70),
+                         generator.race_steps_generator(z2='7km', z3='11km', z4='2km', description='20k race'))
+        self.assertEqual(generator.race_generator('20km', 80),
+                         generator.race_steps_generator(z2='8km', z3='10km', z4='2km', description='20k race'))
+        self.assertEqual(generator.race_generator('20km', 90),
+                         generator.race_steps_generator(z2='9km', z3='9km', z4='2km', description='20k race'))
+        self.assertEqual(generator.race_generator('20km', 100),
+                         generator.race_steps_generator(z2='10km', z3='8km', z4='2km', description='20k race'))
+        self.assertEqual(generator.race_generator('20km', 110),
+                         generator.race_steps_generator(z2='11km', z3='7km', z4='2km', description='20k race'))
+        self.assertEqual(generator.race_generator('20km', 120),
+                         generator.race_steps_generator(z2='12km', z3='6km', z4='2km', description='20k race'))
+        self.assertEqual(generator.race_generator('20km', 130),
+                         generator.race_steps_generator(z2='13km', z3='5km', z4='2km', description='20k race'))
+        self.assertEqual(generator.race_generator('21.1km', 80),
+                         generator.race_steps_generator(z2='8km', z3='11km', z4='2.1km',
+                                                        description='Half marathon race'))
+        self.assertEqual(generator.race_generator('21.1km', 90),
+                         generator.race_steps_generator(z2='9km', z3='10km', z4='2.1km',
+                                                        description='Half marathon race'))
+        self.assertEqual(generator.race_generator('21.1km', 100),
+                         generator.race_steps_generator(z2='10km', z3='9km', z4='2.1km',
+                                                        description='Half marathon race'))
+        self.assertEqual(generator.race_generator('21.1km', 110),
+                         generator.race_steps_generator(z2='11km', z3='8km', z4='2.1km',
+                                                        description='Half marathon race'))
+        self.assertEqual(generator.race_generator('21.1km', 120),
+                         generator.race_steps_generator(z2='12km', z3='7km', z4='2.1km',
+                                                        description='Half marathon race'))
+        self.assertEqual(generator.race_generator('21.1km', 130),
+                         generator.race_steps_generator(z2='13km', z3='6km', z4='2.1km',
+                                                        description='Half marathon race'))
+        self.assertEqual(generator.race_generator('21.1km', 140),
+                         generator.race_steps_generator(z2='14km', z3='5km', z4='2.1km',
+                                                        description='Half marathon race'))
+        self.assertEqual(generator.race_generator('marathon', 150),
+                         generator.race_steps_generator(z1='3km', z2='25km', z3='14.2km', description='Marathon race'))
+        self.assertEqual(generator.race_generator('marathon', 195),
+                         generator.race_steps_generator(z1='7km', z2='25km', z3='10.2km', description='Marathon race'))
+        self.assertEqual(generator.race_generator('marathon', 220),
+                         generator.race_steps_generator(z1='10km', z2='25km', z3='7.2km', description='Marathon race'))
+        self.assertEqual(generator.race_generator('marathon', 240),
+                         generator.race_steps_generator(z1='12km', z2='25km', z3='5.2km', description='Marathon race'))
+        self.assertEqual(generator.race_generator('marathon', 250),
+                         generator.race_steps_generator(z1='15km', z2='25km', z3='2.2km', description='Marathon race'))
 
 
 if __name__ == '__main__':
