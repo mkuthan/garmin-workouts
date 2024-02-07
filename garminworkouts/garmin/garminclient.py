43dcf883659a20612536dae975afa56350036aef
osalgado <osalgado@ikerlan.es>
2024-02-07 22:21:08 +0100

    Bump Garmin version

--- garminworkouts/garmin/garminclient.py
+++ garminworkouts/garmin/garminclient.py
@@ -13,7 +13,7 @@ from garth.exc import GarthHTTPError
 
 class GarminClient(object):
     _GARMIN_SUBDOMAIN = "connectapi"
-    _GARMIN_VERSION = "24.2.2.2"
+    _GARMIN_VERSION = "24.3.0.87"
     _WORKOUT_SERVICE_ENDPOINT = "/workout-service"
     _CALENDAR_SERVICE_ENDPOINT = "/calendar-service"
     _ACTIVITY_SERVICE_ENDPOINT = "/activity-service"
