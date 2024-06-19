c7c8b650bf759c04a31bd46204f96363a6b8c65b
osalgado <osalgado@ikerlan.es>
2024-06-19 22:54:16 +0200

    Add zones test cases

---
+++ tests/models/test_workout.py
@@ -0,0 +1,62 @@
+import unittest
+from datetime import date
+import account
+from garminworkouts.models.workout import Workout
+from garminworkouts.models.pace import Pace
+from garminworkouts.models.power import Power
+
+
+class ZonesTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.workout = Workout(
+            config={},
+            target=[],
+            vVO2=Pace('3:30'),
+            fmin=60,
+            fmax=200,
+            flt=185,
+            rFTP=Power('200'),
+            cFTP=Power('200'),
+            plan='',
+            race=date.today()
+        )
+
+    def test_zones(self) -> None:
+        expected_zones: list[float] = [0.46, 0.6, 0.7, 0.8,
+                                       (account.flt - account.fmin) / (account.fmax-account.fmin),
+                                       1.0, 1.1]
+        expected_hr_zones: list[int] = [107, 127, 141, 155, 167, 183, 196]
+        expected_data: list = [{
+            "changeState": "CHANGED",
+            "trainingMethod": "HR_RESERVE",
+            "lactateThresholdHeartRateUsed": account.flt,
+            "maxHeartRateUsed": account.fmax,
+            "restingHrAutoUpdateUsed": False,
+            "sport": "DEFAULT",
+            "zone1Floor": expected_hr_zones[0],
+            "zone2Floor": expected_hr_zones[1],
+            "zone3Floor": expected_hr_zones[2],
+            "zone4Floor": expected_hr_zones[3],
+            "zone5Floor": expected_hr_zones[4]
+        }]
+
+        zones, hr_zones, data = Workout(
+            [],
+            [],
+            account.vV02,
+            account.fmin,
+            account.fmax,
+            account.flt,
+            account.rFTP,
+            account.cFTP,
+            str(''),
+            date.today()
+        ).hr_zones()
+
+        self.assertEqual(zones, expected_zones)
+        self.assertEqual(hr_zones, expected_hr_zones)
+        self.assertEqual(data, expected_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
