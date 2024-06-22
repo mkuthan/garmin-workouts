75308fa7d3f816807fdfb72e18d5350913a5b311
osalgado <osalgado@ikerlan.es>
2024-06-22 13:42:11 +0200

    Update test_workout

--- tests/models/test_workout.py
+++ tests/models/test_workout.py
@@ -1,6 +1,7 @@
 import unittest
 from datetime import date
 import account
+import logging
 from garminworkouts.models.workout import Workout
 from garminworkouts.models.pace import Pace
 from garminworkouts.models.power import Power
@@ -12,9 +13,9 @@ class ZonesTestCase(unittest.TestCase):
             config={},
             target=[],
             vVO2=Pace('3:30'),
-            fmin=60,
-            fmax=200,
-            flt=185,
+            fmin=account.fmin,
+            fmax=account.fmax,
+            flt=account.flt,
             rFTP=Power('200'),
             cFTP=Power('200'),
             plan='',
@@ -57,6 +58,28 @@ class ZonesTestCase(unittest.TestCase):
         self.assertEqual(zones, expected_zones)
         self.assertEqual(hr_zones, expected_hr_zones)
         self.assertEqual(data, expected_data)
+
+        with self.assertLogs(level=logging.INFO) as cm:
+            self.workout.zones()
+
+        log_messages: list[str] = cm.output
+        self.assertIn('INFO:root:::Heart Rate Zones::', log_messages)
+        self.assertIn(f"INFO:root:fmin: {self.workout.fmin} flt: "
+                      f"{self.workout.flt} fmax: {self.workout.fmax}", log_messages)
+        for i in range(len(expected_zones)):
+            self.assertIn(f"INFO:root: Zone {i}: {expected_hr_zones[i]} - "
+                          f"{expected_hr_zones[i + 1] if i + 1 < len(expected_hr_zones) else 'max'}", log_messages)
+
+        zones, rpower_zones, cpower_zones, _ = Power.power_zones(self.workout.rFTP, self.workout.cFTP)
+        self.assertIn('INFO:root:::Running Power Zones::', log_messages)
+        for i in range(len(rpower_zones)):
+            self.assertIn(f"INFO:root: Zone {i}: {rpower_zones[i]} - "
+                          f"{rpower_zones[i + 1] if i + 1 < len(rpower_zones) else 'max'} w", log_messages)
+
+        self.assertIn('INFO:root:::Cycling Power Zones::', log_messages)
+        for i in range(len(cpower_zones)):
+            self.assertIn(f"INFO:root: Zone {i}: {cpower_zones[i]} - "
+                          f"{cpower_zones[i + 1] if i + 1 < len(cpower_zones) else 'max'} w", log_messages)
 
 
 if __name__ == '__main__':
