13f2497941638deada39e0eb9d68bb28683fca6b
osalgado <osalgado@ikerlan.es>
2024-06-22 20:39:42 +0200

    Reduce platform overload

--- tests/trainingplans/test_workouts.py
+++ tests/trainingplans/test_workouts.py
@@ -5,7 +5,6 @@ from trainingplans.lists import (WorkoutsList, DarebeeLists)
 class TrainingPlanTestCase(BaseTest):
     def test_trainingplan_garmin_workouts(self) -> None:
         self.check_workout_files(WorkoutsList)
-        self.plaftform_workout_files(WorkoutsList)
 
     def test_trainingplan_darebee(self) -> None:
         self.check_workout_files(DarebeeLists)
