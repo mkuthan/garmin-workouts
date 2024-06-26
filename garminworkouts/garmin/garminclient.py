4330bae1e88e6b38f8654e71244399579285f930
OscarSalgado <OscarSalgado@users.noreply.github.com>
2024-06-26 15:08:50 +0000

    Bump Garmin version

--- garminworkouts/garmin/garminclient.py
+++ garminworkouts/garmin/garminclient.py
@@ -10,7 +10,7 @@ import os
 
 class GarminClient(object):
     _GARMIN_SUBDOMAIN = "connectapi"
-    _GARMIN_VERSION = "24.12.1.4"
+    _GARMIN_VERSION = "24.13.0.126"
     _WORKOUT_SERVICE_ENDPOINT = "/workout-service"
     _CALENDAR_SERVICE_ENDPOINT = "/calendar-service"
     _ACTIVITY_SERVICE_ENDPOINT = "/activity-service"
