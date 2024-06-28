451b6aa749980e616391fec613af900e9bb4e0ec
osalgado <osalgado@ikerlan.es>
2024-06-28 22:10:22 +0200

    Test strength

---
+++ tests/models/test_strength.py
@@ -0,0 +1,52 @@
+from garminworkouts.models.strength import (BANDED_EXERCISES, BATTLE_ROPE, BENCH_PRESS, BIKE_OUTDOOR, CALF_RAISE,
+                                            CARDIO, CARRY, CHOP, CORE, CRUNCH, CURL, DEADLIFT, ELLIPTICAL, FLOOR_CLIMB,
+                                            FLYE, HIP_RAISE, HIP_STABILITY, HYPEREXTENSION, LADDER, LATERAL_RAISE,
+                                            LEG_CURL, LEG_RAISE, LUNGE, OLYMPIC_LIFT, PLANK, PLYO, PULL_UP, PUSH_UP,
+                                            ROW, RUN, RUN_INDOOR, SANDBAG, SHOULDER_PRESS, SHOULDER_STABILITY, SHRUG,
+                                            SIT_UP, SLEDGE_HAMMER, SQUAT, STAIR_STEPPER, SUSPENSION, TRICEPS_EXTENSION,
+                                            WARM_UP)
+
+
+def test_strength_model() -> None:
+    assert BANDED_EXERCISES
+    assert BATTLE_ROPE
+    assert BENCH_PRESS
+    assert BIKE_OUTDOOR
+    assert CALF_RAISE
+    assert CARDIO
+    assert CARRY
+    assert CHOP
+    assert CORE
+    assert CRUNCH
+    assert CURL
+    assert DEADLIFT
+    assert ELLIPTICAL
+    assert FLOOR_CLIMB
+    assert FLYE
+    assert HIP_RAISE
+    assert HIP_STABILITY
+    assert HYPEREXTENSION
+    assert LADDER
+    assert LATERAL_RAISE
+    assert LEG_CURL
+    assert LEG_RAISE
+    assert LUNGE
+    assert OLYMPIC_LIFT
+    assert PLANK
+    assert PLYO
+    assert PULL_UP
+    assert PUSH_UP
+    assert ROW
+    assert RUN
+    assert RUN_INDOOR
+    assert SANDBAG
+    assert SHOULDER_PRESS
+    assert SHOULDER_STABILITY
+    assert SHRUG
+    assert SIT_UP
+    assert SLEDGE_HAMMER
+    assert SQUAT
+    assert STAIR_STEPPER
+    assert SUSPENSION
+    assert TRICEPS_EXTENSION
+    assert WARM_UP
