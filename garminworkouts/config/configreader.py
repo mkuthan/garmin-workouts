88f1bfce68fefee0e920c2d403216f5062d73d4a
osalgado <osalgado@ikerlan.es>
2024-07-07 15:55:03 +0200

    Add warning message for context

--- garminworkouts/config/configreader.py
+++ garminworkouts/config/configreader.py
@@ -1,5 +1,6 @@
+import logging
 import yaml
-
+import os
 from garminworkouts.config.includeloader import IncludeLoader
 
 
@@ -10,7 +11,11 @@ def read_config(filename) -> dict:
             i = 0
             for step in data.get('steps', []):
                 if len(step) == 1 and isinstance(step[0], list):
-                    step = step[0]
+                    step: dict = step[0]
                 data['steps'][i] = step
                 i += 1
+            test_name: str = os.path.basename(filename).split('.')[0]
+            if not data['name'] in test_name:
+                logging.warning(f"Name in config file '{data['name']}' does not match filename '{test_name}'")
+
     return data
