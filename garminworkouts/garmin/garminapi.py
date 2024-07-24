46909c863537dea664d021453aecc6bd8d471452
OscarSalgado <OscarSalgado@users.noreply.github.com>
2024-07-24 15:08:59 +0000

    Update Garmin

--- garminworkouts/garmin/garminapi.py
+++ garminworkouts/garmin/garminapi.py
@@ -7,7 +7,7 @@ import os
 
 class GarminApi(object):
     _GARMIN_SUBDOMAIN = "connectapi"
-    _GARMIN_VERSION = "24.15.0.114"
+    _GARMIN_VERSION = "24.15.0.116"
 
     def __init__(self, email, password) -> None:
         self.email: str = email
