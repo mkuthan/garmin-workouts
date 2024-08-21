7dab37455e2bf40ebea7e0795b7e1c2515d62289
osalgado <osalgado@ikerlan.es>
2024-08-21 15:45:21 +0200

    Update file names

--- garminworkouts/config/configreader.py
+++ garminworkouts/config/configreader.py
@@ -7,6 +7,9 @@ from garminworkouts.config.includeloader import IncludeLoader
 def read_config(filename) -> dict:
     with open(filename, 'r') as f:
         data: dict = yaml.load(f, IncludeLoader)
+        test_name: str = os.path.basename(filename).split('.')[0]
+        s: str = data.get('name', '').split('-')[0].replace('D', '_').replace('W0', 'R').replace('W', 'R')
+
         if 'steps' in data:
             i = 0
             for step in data.get('steps', []):
@@ -14,8 +17,13 @@ def read_config(filename) -> dict:
                     step: dict = step[0]
                 data['steps'][i] = step
                 i += 1
-            test_name: str = os.path.basename(filename).split('.')[0]
-            if not data.get('name', '') in test_name and 'goal' not in data:
-                logging.warning(f"Name in config file '{data['name']}' does not match filename '{test_name}'")
 
+            if s not in test_name and 'goal' not in data:
+                logging.warning(f"Name in config file '{s}' does not match filename '{test_name}'")
+        else:
+            if 'content' in data:
+                if '-' in data['name']:
+                    data['name'] = test_name + '-' + data.get('name', '').split('-')[1]
+                else:
+                    data['name'] = test_name + '-' + data.get('name', '')
     return data
