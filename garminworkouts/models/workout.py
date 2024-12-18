c731c03fb80bff7a22ed19e00228902a8213e10f
osalgado <osalgado@ikerlan.es>
2024-12-18 15:49:57 +0100

    refactor: extract workout metrics calculation into a static method for better organization

--- garminworkouts/models/workout.py
+++ garminworkouts/models/workout.py
@@ -80,6 +80,51 @@ class Workout(object):
         if bool(config) and self.mileage == 0 and self.sec == 0 and self.reps == 0:
             raise ValueError('Null workout')
 
+    @staticmethod
+    def load_metrics(workouts):
+        mileage: list[float] = [float(0) for _ in range(24, -11, -1)]
+        duration: list[timedelta] = [timedelta(seconds=0) for i in range(24, -11, -1)]
+        tss: list[float] = [float(0) for _ in range(24, -11, -1)]
+        ECOs: list[float] = [float(0) for _ in range(24, -11, -1)]
+        Rdist = [0] * 8
+        Rdists = [[0] * 8 for _ in range(24, -11, -1)]
+
+        day_min: date | None = None
+        day_max: date | None = None
+
+        for workout in workouts:
+            workout_name: str = workout.get_workout_name()
+            day_d, week, _ = workout.get_workout_date()
+            if day_min is None:
+                day_min = day_d
+            if day_max is None:
+                day_max = day_d
+            if day_min > day_d:
+                day_min = day_d
+            if day_max < day_d:
+                day_max = day_d
+            mileage[week] += workout.mileage
+            duration[week] += workout.duration
+            tss[week] += + workout.tss * workout.duration.seconds
+            ECOs[week] += workout.ECOs
+            Rdist = [r + w_r for r, w_r in zip(Rdist, workout.Rdist)]
+            Rdists[week] = [r + w_r for r, w_r in zip(Rdists[week], workout.Rdist)]
+
+            print(workout_name + ' -',
+                  str(round(workout.mileage, 2)) + ' km -',
+                  str(workout.duration) + ' -',
+                  str(round(workout.ECOs, 2)) + ' ECOs')
+
+        logging.info('From ' + str(day_min) + ' to ' + str(day_max))
+        for i in range(24, -11, -1):
+            if mileage[i] > float(0):
+                logging.info('Week ' + str(i) + ': '
+                             + str(round(mileage[i], 2)) + ' km - '
+                             + 'Duration: ' + str(duration[i]) + ' - '
+                             + 'ECOs: ' + str(round(ECOs[i], 2)))
+
+        return mileage, duration, tss, ECOs, Rdist, Rdists, day_min, day_max
+
     def zones(self) -> None:
         zones, hr_zones, _ = self.hr_zones()
         logging.info('::Heart Rate Zones::')
