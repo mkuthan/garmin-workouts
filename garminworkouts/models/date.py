5ce2ada269e5b03c1cd3af3260ab5113b9a0d656
osalgado <osalgado@ikerlan.es>
2025-01-03 08:52:09 +0100

    Refactor date parsing logic in get_date function for improved readability and efficiency

--- garminworkouts/models/date.py
+++ garminworkouts/models/date.py
@@ -3,16 +3,9 @@ from datetime import date, timedelta
 
 def get_date(name, race, date_ini) -> tuple[date, int, int]:
     try:
-        if name.startswith('R'):
-            week: int = -int(name[1:name.index('_')])
-        elif '_' in name:
-            week = int(name[0:name.index('_')])
-        else:
-            week = -int(name[1:name.index('D')])
-        if '_' in name:
-            day = int(name[name.index('_') + 1:name.index('_') + 2])
-        else:
-            day = int(name[name.index('D') + 1:name.index('-')])
+        week: int = int(name[1:name.index('_')]) if name.startswith('R') else int(name.split('_')[0])
+        week: int = -week if name.startswith('R') or 'D' in name else week
+        day: int = int(name.split('_')[1][0]) if '_' in name else int(name.split('D')[1].split('-')[0])
         return race - timedelta(weeks=week + 1) + timedelta(days=day), week, day
     except ValueError:
         if isinstance(date_ini, dict):
